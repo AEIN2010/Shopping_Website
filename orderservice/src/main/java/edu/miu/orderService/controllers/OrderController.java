daca99ce289d8c2040dd22763bcd7284cd2d8134
Lam Thai Thanh Long <lamthaithanhlong@gmail.com>
2023-05-25 08:06:56 -0500

    update send mail with JWT

--- orderservice/src/main/java/edu/miu/orderService/controllers/OrderController.java
+++ orderservice/src/main/java/edu/miu/orderService/controllers/OrderController.java
@@ -1,14 +1,19 @@
 package edu.miu.orderService.controllers;
 
+import java.util.List;
+
 import edu.miu.orderService.JwtUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import edu.miu.orderService.models.Order;
+import edu.miu.orderService.models.Payment;
 import edu.miu.orderService.services.OrderService;
 import edu.miu.orderService.services.RabbitMQSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+// Import the Status enum
+import edu.miu.orderService.models.Status;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -24,9 +29,8 @@ public class OrderController {
     public List<Order> getOrders() {
         return orderService.getOrders();
     }
-
     @GetMapping("/byid/{userId}")
-    public List<Order> getOrdersById(@PathVariable String userId) {
+    public List<Order> getOrdersById(@PathVariable String userId){
         return orderService.findOrdersByUserId(userId);
     }
 
@@ -34,8 +38,10 @@ public class OrderController {
     public Order createOrder(@PathVariable String userId, @RequestHeader("Authorization") String token) {
         Order order = orderService.createOrder(userId);
 
-        // Send email for created order
-        token = JwtUtil.removeBearerPrefix(token);
+        // Send mail for created order
+        // Remove the "Bearer " prefix from the token
+        token = token.replace("Bearer ", "");
+        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDZlZWM5ZmEzMTRmZDI5MjQzZjM1ZmMiLCJ1c2VybmFtZSI6InJ0MSIsImVtYWlsIjoicnRAcnQuY29tIiwicGFzc3dvcmQiOiIiLCJfX3YiOjAsImlhdCI6MTY4NTAxNzUzMCwiZXhwIjoxNjg1MDIxMTMwfQ.G55bv48gZ2B05usN8oGoXMH9TT9q4tLa0nHo0SmhMFU";
         String email = JwtUtil.extractEmailFromToken(token);
 
         rabbitMQSenderService.sendOrderConfirmation(
@@ -57,12 +63,11 @@ public class OrderController {
     @PutMapping
     public Order updateOrder(@RequestBody Order order, @RequestHeader("Authorization") String token) {
         Order updatedOrder = orderService.updateOrder(order);
-        token = JwtUtil.removeBearerPrefix(token);
+        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDZlZWM5ZmEzMTRmZDI5MjQzZjM1ZmMiLCJ1c2VybmFtZSI6InJ0MSIsImVtYWlsIjoicnRAcnQuY29tIiwicGFzc3dvcmQiOiIiLCJfX3YiOjAsImlhdCI6MTY4NTAxNzUzMCwiZXhwIjoxNjg1MDIxMTMwfQ.G55bv48gZ2B05usN8oGoXMH9TT9q4tLa0nHo0SmhMFU";
         String email = JwtUtil.extractEmailFromToken(token);
-
         // Check if the order status is updated to DELIVERED
-        if (updatedOrder.getStatus() == Status.DELIVERED) {
-            // Send email for delivery order
+        if(updatedOrder.getStatus() == Status.DELIVERED) {
+            // Send mail for delivery order
             rabbitMQSenderService.sendDeliveryConfirmation(
                     "sender@example.com",
                     List.of(email),
@@ -71,7 +76,6 @@ public class OrderController {
                     List.of()
             );
         }
-
         return updatedOrder;
     }
 
