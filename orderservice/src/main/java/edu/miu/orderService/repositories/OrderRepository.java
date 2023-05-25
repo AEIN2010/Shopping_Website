cf5e01a5c571fe68172632d52ebf76e8e85ed032
Daniel Tsegay Meresie <dani.2tse@gmail.com>
2023-05-25 01:22:13 -0500

    find by userId completed

--- orderservice/src/main/java/edu/miu/orderService/repositories/OrderRepository.java
+++ orderservice/src/main/java/edu/miu/orderService/repositories/OrderRepository.java
@@ -3,10 +3,12 @@ package edu.miu.orderService.repositories;
 import edu.miu.orderService.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  *
  * @author Daniel Tsegay Meresie
  */
 public interface OrderRepository extends JpaRepository<Order, Long>{
-    
+    List<Order> findByUserId(int userId);
 }
